b61efe15f6a321770a1b8b57e9655076fbead471
Tobias Kuhn <kuhntobias@gmail.com>
2014-01-24 08:04:56 +0100

    Support for downloading different releases

--- run.sh
+++ run.sh
@@ -1,36 +1,53 @@
 #!/bin/bash
+#
+# Usage (specifying release):
+#
+#     $ ./run.sh 2
+#
+# Usage (default release):
+#
+#     $ ./run.sh
+#
 
-URL=http://download.bio2rdf.org/release/2/
+RELEASE=2
 
-rm -f err.log out.log
+if [ "$1" != "" ] ; then
+  RELEASE=$1
+fi
 
-mkdir -p files
-cd files
+URL=http://download.bio2rdf.org/release/$RELEASE/
 
-echo "Downloading Index..." >> ../out.log
+DIR=files/$RELEASE
 
-wget $URL -O index.html >> ../out.log 2>> ../err.log
+rm -f $DIR/err.log $DIR/out.log
+
+mkdir -p $DIR
+cd $DIR
+
+echo "Downloading Index..." >> out.log
+
+wget $URL -O index.html >> out.log 2>> err.log
 
 cat index.html | egrep '<a href="[^"]*">[^<].*/</a>' | sed 's/^.*<a href="[^"]*">\([^<].*\)\/<\/a>.*$/\1/' > dirs.txt
 rm index.html
 
 while read D; do
-  echo "Accessing $D..." >> ../out.log
+  echo "Accessing $D..." >> out.log
   mkdir -p $D
   cd $D
-  wget ${URL}${D}/ -O index.html >> ../../out.log 2>> ../../err.log
+  wget ${URL}${D}/ -O index.html >> ../out.log 2>> ../err.log
   cat index.html | egrep '<a href="[^"]*">[^<].*(\.nt|\.owl)(\.gz)?</a>' | sed 's/^.*<a href="[^"]*">\([^<].*\)<\/a>.*$/\1/' > files.txt
   rm index.html
   while read F; do
-    echo "Downloading $F..." >> ../../out.log
-    wget ${URL}${D}/${F} >> ../../out.log 2>> ../../err.log
+    echo "Downloading $F..." >> ../out.log
+    wget ${URL}${D}/${F} >> ../out.log 2>> ../err.log
   done < files.txt
-  echo "Uncompressing files..." >> ../../out.log
-  gunzip *.gz >> ../../out.log 2>> ../../err.log
+  echo "Uncompressing files..." >> ../out.log
+  gunzip *.gz >> ../out.log 2>> ../err.log
   rm files.txt
   cd ..
 done < dirs.txt
 
 rm dirs.txt
 
-echo "Finished." >> ../out.log
+echo "Finished." >> out.log
